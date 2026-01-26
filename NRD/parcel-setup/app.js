aec9b5dcbd70a86b815671c6360efdd538c540c3
Rudra Verma <rudraverma781@gmail.com>
2026-01-26 17:04:35 +0530

    some new things added !!!!

--- NRD/parcel-setup/app.js
+++ NRD/parcel-setup/app.js
@@ -6,4 +6,46 @@ const heading = React.createElement("h1",{id:"heading"}, " Hello from Parcel !")
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(heading)
+// root.render(heading)
+
+
+const head2 = <h1 id="heading"> Hello from Parcel !</h1>
+
+const Header = (<div>
+    {head2}
+     <span>again Hy !!</span>
+</div>)
+
+// root.render(Header)
+
+
+// functional components
+
+const Welcome = ()=>{
+    return (
+        <div>
+            <h1> Welcome to React !</h1>
+            <p> Hello from React !</p>
+        </div>
+    )
+}
+
+
+const Bye =()=> (
+    <div>
+        <Welcome/>
+        <h1> Bye Bye !</h1>
+    </div>
+)
+
+const Shake =()=> (
+    <div>
+        {Bye()}
+        <h1> Hand-Shake </h1>
+    </div>
+)
+
+
+root.render(<Shake/>)
+
+
