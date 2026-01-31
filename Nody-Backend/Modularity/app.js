2112c4327659638548f5568d0c1bbad7da93a49d
Rudra Verma <rudraverma781@gmail.com>
2026-01-31 15:26:30 +0530

    some new things added !!!!

---
+++ Nody-Backend/Modularity/app.js
@@ -0,0 +1,9 @@
+
+const {sum} = require("./xyz")
+// require("./xyz")
+// require("./xyz")
+// require("./xyz")
+// require("./xyz")
+ sum(10,20)
+
+console.log("APP JS !! ")
---
+++ Nody-Backend/Modularity/xyz.js
@@ -0,0 +1,8 @@
+console.log("XYZ")
+
+function sum(a,b){
+    console.log("SUM : ",a," + ",b+" : ",a+b);
+    
+}
+
+module.exports = {sum};
