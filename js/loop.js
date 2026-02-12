027a5489cda984f2f78882c51baf59939d1c7413
Rudra Verma <rudraverma781@gmail.com>
2026-02-12 18:28:41 +0530

    some new things added !!!!

---
+++ js/loop.js
@@ -0,0 +1,36 @@
+// for(let i =0;i<10;i++)
+// {
+//     console.log(i)
+// }
+
+
+
+// x=10
+// while(x>0){
+//     console.log("While loop iteration -"+x)
+//     x--
+// }
+
+
+
+// do{
+//     console.log("hello")
+//     x++
+// }while(x<10)
+
+// for table 
+//let x =prompt("Enter a Number :")
+// for(let i = 1;i<=10;i++){
+//     console.log(x+" X "+i+" = "+x*i);
+    
+// }
+
+
+Math.round(3.15)
+Math.ceil(1.0000001)
+Math.floor(2.1)
+Math.pow(2,3)
+Math.sqrt(121)
+
+
+
