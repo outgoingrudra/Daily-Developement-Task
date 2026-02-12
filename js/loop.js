f5b4e24f9c85d383afe1c728a47775813c6b8f39
Rudra Verma <rudraverma781@gmail.com>
2026-02-12 19:10:53 +0530

    some new things added !!!!

--- js/loop.js
+++ js/loop.js
@@ -31,6 +31,59 @@ Math.ceil(1.0000001)
 Math.floor(2.1)
 Math.pow(2,3)
 Math.sqrt(121)
+Math.max(12,11,5,24,23)
+Math.min(11,23,1)
+Math.random()
 
 
+
+// let x  = 67
+// let ur 
+// do{
+//    ur = prompt("Enter a Number :")
+//   if(ur<x)
+//      alert("Your Number is less than X")
+//   else if(ur>x)
+//      alert("Your Number is greater than X")
+//   else
+//      alert("Congrutulations ! You nailed it ! ")
+  
+
+// }while(ur!=x)
+
+
+// reverse a string 
+
+let str ="Hello World"
+let ans =""
+for(let i =0;i<str.length;i++)
+{
+    ans = str[i]+ans
+}
+console.log(ans);
+
+
+
+
+// count vowels & consonants 
+let s ="watErmelon"
+let cnt =0
+for(let i =0;i<s.length;i++)
+{
+    let x = s[i].toLowerCase()
+    if(x=='a' || x=='e' || x=='i' || x=='o' || x=='u')
+        cnt++
+}
+console.log("total vowels :"+cnt);
+console.log("total consonants :"+(s.length-cnt));
+
+
+
+let name ="Abdul Pakir Janul Abdul Kalam"  // APJAK
+
+let name_arr = name.split(" ")
+ans = ""
+for(let i = 0;i<name_arr.length ;i++)
+      ans= ans + name_arr[i][0]
+console.log(ans);
 
