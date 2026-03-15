a2fcdc8773a1aeeb4648e7b02aa8b3cc08cfae5f
Rudra Verma <rudraverma781@gmail.com>
2026-03-15 11:56:02 +0530

    some new things added !!!!

---
+++ js/async-basics.js
@@ -0,0 +1,9 @@
+
+console.log("Starting time : "+new Date())
+
+setTimeout(() => {
+    console.log("Waited for 5 second ")
+    console.log("Finishing time : "+new Date())
+}, 5000);
+
+console.log("ended !")
