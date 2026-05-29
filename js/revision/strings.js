b7765ab017a3cb4d335972e0b440a11db6a6d08d
Rudra Verma <rudraverma781@gmail.com>
2026-05-29 21:14:34 +0530

    some new things added !!!!

---
+++ js/revision/strings.js
@@ -0,0 +1,11 @@
+let str = "supercalifragilisticexpalidocious   "
+
+console.log(str.length)
+console.log(str.charAt(1))
+console.log(str.indexOf("o"))
+console.log(str.lastIndexOf("s"))
+console.log(str.trim());
+console.log(str.toUpperCase())
+console.log(str.slice(0,3))
+
+
