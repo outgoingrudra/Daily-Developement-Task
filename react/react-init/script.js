e6dc94bec6e664435e9ac02910fa0f10df3983c8
Rudra Verma <rudraverma781@gmail.com>
2026-06-05 20:49:12 +0530

    some new things added !!!!

---
+++ react/react-init/script.js
@@ -0,0 +1,12 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+const heading = React.createElement("h3",{ id : "heading"},"Heading ")
+const input = React.createElement("input",{placeholder:"Enter Name"})
+
+const main = React.createElement("div",{},[ heading , input ])
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(main)
+
